14a7d549520b5ad3ac02ca157738f84a320349e7
Patrick Williams <patrickw@porch.com>
2016-12-08 13:25:33 -0800

    use error directly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import newrelic from 'newrelic';
 import { EventEmitter } from 'events';
 
 export default class extends EventEmitter {
-    write({ msg, ...options }) {
-        newrelic.noticeError(new Error(msg), options);
+    write(error) {
+        newrelic.noticeError(error);
     }
 }
